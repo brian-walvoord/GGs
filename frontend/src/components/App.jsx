5575da2f45e0daf3c63f067079ccd132c59bb710
Brian Walvoord <brian.walvoord@gmail.com>
2022-11-08 21:20:54 +0900

    feat: added simple get route from frontend to api

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -4,6 +4,14 @@ import React, { useState, useEffect } from "react";
 function App() {
 
   const [hello, setHello] = useState(null);
+  const [games, setGames] = useState(null);
+
+  const fetchGames = () => {
+    fetch("/getGames")
+      .then(res => res.json())
+      // .then(res => console.log(res))
+      .then(res => setGames(res))
+  };
 
   useEffect(() => {
     fetch("/api")
@@ -15,6 +23,8 @@ function App() {
     <div>
       <h1>Hello, World!</h1>
       <h1>{hello}</h1>
+      <button onClick={fetchGames}>Get Games</button>
+      {games ? games.map(game => <h1 key={game.id}>{JSON.stringify(game)}</h1>) : null}
     </div>
   );
 }
