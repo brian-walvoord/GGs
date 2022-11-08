73c0a568a0bb752a400890c85226d8eed7905146
Brian Walvoord <brian.walvoord@gmail.com>
2022-11-08 20:06:02 +0900

    feat: added all get route and static path in server.js for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const path = require("path");
+app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+
 app.use(express.json());
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`🎉server listening on port: ${PORT}`);
