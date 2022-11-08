c4dd6619a69b264f714eadef91d54a92d50d0966
Brian Walvoord <brian.walvoord@gmail.com>
2022-11-08 20:28:40 +0900

    feat: connected fe and be, commented out path in server.js for dev purposes

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const path = require("path");
-app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+// const path = require("path");
+// app.use(express.static(path.resolve(__dirname, "../frontend/build")));
 
 app.use(express.json());
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
+app.get("/api", (req, res) => {
+  try {
+    res.status(200).json("👋Hello from the api");
+  } catch (err) {
+    console.log(err);
+  }
 });
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`🎉server listening on port: ${PORT}`);
