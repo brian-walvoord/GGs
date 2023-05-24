17b91c6dbbb2119386b09edd0770f5d52b5c5937
Brian Walvoord <brian.walvoord@gmail.com>
2023-05-24 09:56:57 +0900

    style adjustment

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -16,47 +16,47 @@ function App() {
   const [gamePopup, setGamePopup] = useState(false);
   const [libraryPopup, setLibraryPopup] = useState(false);
   const [selection, setSelection] = useState(null);
-  const [selectedUser, setSelectedUser] = useState(localStorage.getItem("id"))
+  const [selectedUser, setSelectedUser] = useState(localStorage.getItem("id"));
   const [user, setUser] = useState("");
   const [library, setLibrary] = useState(null);
   const [userCreated, setUserCreated] = useState(false);
 
   return (
     <div className="app-container">
-        <Header user={user} setUser={setUser} />
-        <Routes>
-          <Route path="/" element={<UserSelect setSelectedUser={setSelectedUser} userCreated={userCreated}/>} />
-          <Route path="/home" element={<><Navbar setUser={setUser}/>
-            <Homepage 
-              user={user} 
-              setUser={setUser} 
-              selectedUser={selectedUser}
-            /></>} />
-          <Route path="/games" element={<>
-            <Navbar setUser={setUser} />
-            <Games 
-              games={games} 
-              setGames={setGames} 
-              gamePopup={gamePopup} 
-              setGamePopup={setGamePopup}
-              selection={selection}
-              setSelection={setSelection}
-              user={user}
-            />
-          </>} />
-          <Route path="/library" element={<>
-            <Navbar setUser={setUser} />
-            <Library 
-              selection={selection}
-              setSelection={setSelection}
-              setLibraryPopup={setLibraryPopup}
-              libraryPopup = {libraryPopup}
-              library={library}
-              setLibrary={setLibrary}
-            />
-          </>} />
-          <Route path="/signup" element={<Signup setUserCreated={setUserCreated}/>} />
-        </Routes>
+      <Header user={user} setUser={setUser} />
+      <Routes>
+        <Route path="/" element={<UserSelect setSelectedUser={setSelectedUser} userCreated={userCreated} />} />
+        <Route path="/home" element={<><Navbar setUser={setUser} />
+          <Homepage
+            user={user}
+            setUser={setUser}
+            selectedUser={selectedUser}
+          /></>} />
+        <Route path="/games" element={<>
+          <Navbar setUser={setUser} />
+          <Games
+            games={games}
+            setGames={setGames}
+            gamePopup={gamePopup}
+            setGamePopup={setGamePopup}
+            selection={selection}
+            setSelection={setSelection}
+            user={user}
+          />
+        </>} />
+        <Route path="/library" element={<>
+          <Navbar setUser={setUser} />
+          <Library
+            selection={selection}
+            setSelection={setSelection}
+            setLibraryPopup={setLibraryPopup}
+            libraryPopup={libraryPopup}
+            library={library}
+            setLibrary={setLibrary}
+          />
+        </>} />
+        <Route path="/signup" element={<Signup setUserCreated={setUserCreated} />} />
+      </Routes>
       <div className="background"></div>
     </div>
   );
